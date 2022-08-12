0fd52f248b842fe391d2f0c62ce10508a50da370
max <badasskid@me.com>
2022-08-12 14:50:51 +0700

    Added Reading Player Input

--- Assets/Scripts/LevelScripts/CellComponent.cs
+++ Assets/Scripts/LevelScripts/CellComponent.cs
@@ -1,29 +1,31 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 
 namespace Match3
 {
-    public class CellComponent : MonoBehaviour
+    public class CellComponent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField]
         private ChipComponent[] _chipPrefabs;
         public ChipComponent Chip;
         private Dictionary<DirectionType, CellComponent> _neighbours;
-
         private int _layer;
         public event Action<ChipComponent> PoolingEvent;
+        public event Action<CellComponent,Vector2> PointerDownEvent;
+        public event Action<CellComponent> PointerUpEvent;
 
         private void Start()
         {
             _layer = LayerMask.GetMask($"Level");
             _neighbours = new Dictionary<DirectionType, CellComponent>();
             FindNeighbours();
-            GenerateChip();
+            StartCoroutine(GenerateChipRoutine());
         }
+
         private void FindNeighbours()
         {
             RaycastHit2D topRay = Physics2D.Raycast(transform.position, transform.up, 1f, _layer);
@@ -37,39 +39,31 @@ namespace Match3
             if (rightRay.collider != null) _neighbours.Add(DirectionType.Right, rightRay.collider.GetComponent<CellComponent>());
         }
 
-        private async void GenerateChip()
+        private IEnumerator GenerateChipRoutine()
         {
-            // var allowedChips = _chipPrefabs.Where(z => !_neighbours
-            //                                             .Where(x => x.Value.Chip != null)
-            //                                             .Select(c => c.Value.Chip.Type)
-            //                                             .Distinct()
-            //                                             .Contains(z.Type)).ToArray();
-            //
-            // ChipComponent randomChip = UnityEngine.Random.value >= 0.5f
-            //     ? _chipPrefabs[UnityEngine.Random.Range(0, _chipPrefabs.Length)]
-            //     : allowedChips[UnityEngine.Random.Range(0, allowedChips.Length)];
-
             Chip = Instantiate(_chipPrefabs[UnityEngine.Random.Range(0, _chipPrefabs.Length)], transform);
 
-            await Task.Yield();
+            yield return null;
 
             var vertical = _neighbours.Where(z => z.Key == DirectionType.Bot || z.Key == DirectionType.Top).ToArray();
             var horizontal = _neighbours.Where(z => z.Key == DirectionType.Left || z.Key == DirectionType.Right).ToArray();
 
             while (vertical.All(z => z.Value.Chip.Type == Chip.Type) || horizontal.All(z => z.Value.Chip.Type == Chip.Type))
             {
-                print($"{name } + ");
                 Chip.gameObject.SetActive(false);
                 PoolingEvent?.Invoke(Chip);
 
-                var newAllowedChips = _chipPrefabs.Where(z => !horizontal
+                var allowedChips = _chipPrefabs.Where(z => !horizontal
                                                                .Union(vertical)
                                                                .Distinct()
                                                                .Select(x => x.Value.Chip.Type)
                                                                .Contains(z.Type)).ToArray();
 
-                Chip = Instantiate(newAllowedChips[UnityEngine.Random.Range(0, newAllowedChips.Length)], transform);
+                Chip = Instantiate(allowedChips[UnityEngine.Random.Range(0, allowedChips.Length)], transform);
             }
         }
+
+        public void OnPointerDown(PointerEventData eventData) => PointerDownEvent?.Invoke(this,eventData.position);
+        public void OnPointerUp(PointerEventData eventData) => PointerUpEvent?.Invoke(this);
     }
 }
--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Match3
@@ -12,22 +10,77 @@ namespace Match3
         [SerializeField]
         private CellComponent[] _cells;
         private LinkedList<ChipComponent> _chipPool;
-        //todo убрать
-        public int Count;
+        private Controls _controls;
+        private Vector2 _startDragMousePos;
+        private bool _isReading;
+        private CellComponent _currentCell;
+        [SerializeField]
+        private float _dragSens;
 
         private void Start()
         {
+            _controls = new Controls();
+            _controls.Enable();
             _chipPool = new LinkedList<ChipComponent>();
+            _startDragMousePos = Vector2.zero;
 
             foreach (var cell in _cells)
             {
                 cell.PoolingEvent += OnPoolingEvent;
+                cell.PointerDownEvent += OnCellPointerDownEvent;
+                cell.PointerUpEvent += OnCellPointerUpEvent;
             }
         }
+
+        private void OnCellPointerUpEvent(CellComponent cell) => _isReading = false;
+
+        private void OnCellPointerDownEvent(CellComponent cell, Vector2 cellPos)
+        {
+            _currentCell = cell;
+            _isReading = true;
+            _startDragMousePos = cellPos;
+        }
+
+        private void Update()
+        {
+            ReadPlayerInput();
+        }
+
+        private void ReadPlayerInput()
+        {
+            if (!_isReading) return;
+            Vector2 newMousePos = _controls.MainMap.Mouse.ReadValue<Vector2>();
+
+            if (newMousePos.x - _startDragMousePos.x > _dragSens)
+            {
+                _isReading = false;
+                print("Right");
+            }
+            else if (newMousePos.x - _startDragMousePos.x < -_dragSens)
+            {
+                _isReading = false;
+                print("Left");
+            }
+            else if (newMousePos.y - _startDragMousePos.y > _dragSens)
+            {
+                _isReading = false;
+                print("Top");
+            }
+            else if (newMousePos.y - _startDragMousePos.y < -_dragSens)
+            {
+                _isReading = false;
+                print("Bot");
+            }
+        }
+
         private void OnPoolingEvent(ChipComponent chip)
         {
             _chipPool.AddLast(chip);
-            Count = _chipPool.Count();
+        }
+
+        private void OnDestroy()
+        {
+            _controls.Dispose();
         }
     }
 }
